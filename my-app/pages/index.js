c80c0df058fce42f16437726c56f5f3f054cd5a1
Daniel Izevbije <dizevbije94@gmail.com>
2023-03-24 14:01:10 +0100

    first commit

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -334,7 +334,7 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className={styles.footer}>Made with &#10084; by Your Name</footer>
+      <footer className={styles.footer}>Made with &#10084; by Dan</footer>
     </div>
   );
 }
